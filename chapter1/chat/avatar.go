00ec983c08836eb9b5e5873f814e9af44390f56b
mac-222 <mac-222@mac-222s-Mac-mini.local>
2019-02-11 16:47:34 +0300

    added test fle

--- chapter1/chat/avatar.go
+++ chapter1/chat/avatar.go
@@ -4,7 +4,7 @@ import "errors"
 
 // ErrNoAvatar is the error that is returned when the
 // Avatar instance is unable to provide an avatar URL.
-var ErrNoAvatar = errors.New("chat: Unable to get an avatar URL.")
+var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL.")
 
 // Avatar represents types capable of representing
 // user profile pictures.
@@ -14,4 +14,19 @@ type Avatar interface {
 	// ErrNoAvatarURL is returned if the object is unable to get
 	// a URL for the specified client.
 	GetAvatarURL(c *client) (string, error)
+}
+
+type AuthAvatar struct {
+}
+
+var UseAuthAvatar AuthAvatar
+
+func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+
+	return "", ErrNoAvatarURL
 }
