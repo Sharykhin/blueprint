9c717ea93a3d355252cb7e7d1adf559cb7936fc8
mac-222 <mac-222@mac-222s-Mac-mini.local>
2019-02-14 15:50:26 +0300

    working on mongo

--- chapter5/socialpoll/twittervotes/main.go
+++ chapter5/socialpoll/twittervotes/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
+
+var db *mgo.Session
+
 func main() {
 
+}
+
+func dialdb() error {
+	var err error
+	log.Println("dialing mongodb: localhost:27018")
+	db, err := mgo.Dial("localhost:27018")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("closed database connection")
 }
--- chapter5/socialpoll/twittervotes/twitter.go
+++ chapter5/socialpoll/twittervotes/twitter.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"github.com/joeshaw/envdecode"
+	"github.com/matryer/go-oauth/oauth"
 	"io"
+	"log"
 	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
 	"time"
 )
 
 var conn net.Conn
 var reader io.ReadCloser
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
 
 func dial(netw, addr string) (net.Conn, error) {
 	if conn != nil {
@@ -32,4 +49,48 @@ func closeConn() {
 	if reader != nil {
 		reader.Close()
 	}
+}
+
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY,required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN,required"`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET,required"`
+	}
+
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+}
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+
+	return httpClient.Do(req)
+
 }
