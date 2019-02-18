77b0ae0d0d49b08b71a3f0d65fc06f9a6b86425b
mac-222 <mac-222@mac-222s-Mac-mini.local>
2019-02-18 15:30:13 +0300

    working on

--- chapter5/socialpoll/twittervotes/main.go
+++ chapter5/socialpoll/twittervotes/main.go
@@ -1,24 +1,79 @@
 package main
 
 import (
+	"github.com/bitly/go-nsq"
 	"gopkg.in/mgo.v2"
 	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 var db *mgo.Session
 
-func main() {
+type poll struct {
+	Options []string
+}
 
+func main() {
+	var stoplock sync.Mutex
+	stop := false
+	stopChan := make(chan struct{}, 1)
+	signalChan := make(chan os.Signal, 1)
+
+	go func() {
+		<-signalChan
+		stoplock.Lock()
+		stop = true
+		stoplock.Unlock()
+		log.Println("Stopping...")
+		stopChan <- struct{}{}
+		closeConn()
+	}()
+
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 }
 
 func dialdb() error {
 	var err error
 	log.Println("dialing mongodb: localhost:27018")
-	db, err := mgo.Dial("localhost:27018")
+	db, err = mgo.Dial("localhost:27018")
 	return err
 }
 
 func closedb() {
 	db.Close()
 	log.Println("closed database connection")
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	defer iter.Close()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+
+	return options, iter.Err()
+
+}
+
+func publishVotes(votes <-chan string) <-chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+
+	go func() {
+		for vote := range votes {
+			pub.Publish("votes", []byte(vote))
+		}
+
+		log.Println("Publisher: Stopping")
+		pub.Stop()
+		log.Println("Publisher: Stopped")
+		stopchan <- struct{}{}
+	}()
+
+	return stopchan
 }
