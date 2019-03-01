09c4e50544819499f093a44209a749fcc07e15d9
nb-042-71b <nb-042-71b@nb-042-71bs-MacBook-Pro.local>
2019-03-01 16:37:54 +0300

    changed server just a bit

--- chapter7/meander/cmd/meander/main.go
+++ chapter7/meander/cmd/meander/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/Sharykhin/blueprint/chapter7/meander"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -13,15 +17,34 @@ func main() {
 		respond(w, r, meander.Journeys)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", http.DefaultServeMux))
+	server := http.Server{
+		Addr:         ":8080",
+		Handler:      http.DefaultServeMux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	go func() {
+		sigs := make(chan os.Signal)
+		signal.Notify(sigs, os.Interrupt)
+		sig := <-sigs
+		log.Printf("Server has been interrupted: %v", sig)
+		err := server.Shutdown(context.Background())
+		if err != nil {
+			log.Fatalf("Could not gracefully shutdown server: %v", err)
+		}
+	}()
+
+	log.Println("Server started on", ":8080")
+	log.Fatal(server.ListenAndServe())
 }
 
-func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
+func respond(w http.ResponseWriter, r *http.Request, data []interface{}) {
 	publicData := make([]interface{}, len(data))
 
 	for i, d := range data {
 		publicData[i] = meander.Public(d)
 	}
 
-	return json.NewEncoder(w).Encode(publicData)
+	json.NewEncoder(w).Encode(publicData)
 }
