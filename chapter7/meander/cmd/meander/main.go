0dab510abdf93310692bc63dadd3e655c27d40e0
nb-042-71b <nb-042-71b@nb-042-71bs-MacBook-Pro.local>
2019-03-13 10:36:04 +0300

    added some more checks

--- chapter7/meander/cmd/meander/main.go
+++ chapter7/meander/cmd/meander/main.go
@@ -8,14 +8,41 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	meander.APIKey = "AIzaSyDhzVMK5Yj-SnY2HgveUPNpia31rM1RHPY"
-	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/journeys", cors(func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
-	})
+	}))
+
+	http.HandleFunc("/recommendations", cors(func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		var err error
+		q.Lat, err = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Lng, err = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Radius, err = strconv.Atoi(r.URL.Query().Get("radius"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	}))
 
 	server := http.Server{
 		Addr:         ":8080",
@@ -37,6 +64,13 @@ func main() {
 
 	log.Println("Server started on", ":8080")
 	log.Fatal(server.ListenAndServe())
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		f(w, r)
+	}
 }
 
 func respond(w http.ResponseWriter, r *http.Request, data []interface{}) {
