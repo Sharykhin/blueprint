83423075b8548337ab615ee1998e5efd2c2f00b9
nb-042-71b <nb-042-71b@nb-042-71bs-MacBook-Pro.local>
2019-03-14 17:42:07 +0300

    added archiver

--- chapter8/backup/archiver.go
+++ chapter8/backup/archiver.go
@@ -8,6 +8,7 @@ import (
 )
 
 type Archiver interface {
+	DestFmt() string
 	Archive(src, dest string) error
 }
 
@@ -15,6 +16,10 @@ type zipper struct {
 }
 
 var ZIP Archiver = (*zipper)(nil)
+
+func (z *zipper) DestFmt() string {
+	return "%d.zip"
+}
 
 func (z *zipper) Archive(src, dest string) error {
 	if err := os.MkdirAll(filepath.Dir(dest), 0777); err != nil {
